533bb7a5478ce65f2c87a228c645236020e799f7
mfurkanyener <m.furkanyener@hotmail.com>
2025-03-08 12:45:25 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `mfurkayener.com`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag"]
+};
