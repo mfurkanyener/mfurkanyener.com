0add0a142fb6f3fef21c3672884d5a457b7a82f5
mfurkanyener <m.furkanyener@hotmail.com>
2025-03-13 01:57:57 +0300

    Remove sensitive files from the cache

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-
-export const wrapRootElement = ({ element }) => {
-    return <Router>{element}</Router>;
-};
